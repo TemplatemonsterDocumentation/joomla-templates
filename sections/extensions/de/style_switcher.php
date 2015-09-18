fde574d840c4436612cfc7093b7a3da40aa50a81
dok <dok@devoffice.com>
2015-09-18 12:45:26 +0300

    add style switcher, facebook.

---
+++ sections/extensions/de/style_switcher.php
@@ -0,0 +1,15 @@
+<h3>TM Ajax Style Switcher Module</h3>
+
+<p><strong>TM Ajax Style Switcher Module</strong> – ist ein Modul, das ermöglicht es, Farbschemas im Frontend der Webseite umzuschalten.
+    Das Modul verwendet automatisch Farbschemas aus dem Ordner color_schemes\css\ vom aktiven Theme.</p>
+
+<p>Das jeweilige Modul hat keine speziellen Einstellungen im Control Panel. Es hat nur Standardeinstellungen, die alle
+    Module haben.</p>
+
+<figure class="img-polaroid">
+    <img src="img/style_switcher_1.jpg" alt="">
+</figure>
+
+<p class="alert alert-warning">Achten Sie darauf, dass dieses Modul auf der Demoversion im Demo-Modus (mit Hilfe von
+    Cookies) funktionieren kann. Auf der aktuellen Website funktioniert das Modul im Vollmodus und ist nur für
+    eingeloggte Benutzer mit den Rechten der Benutzerkategorien SuperAdmin verfügbar.</p>
