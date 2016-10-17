0db849dbc1c3bca70c4f546c7c3677f22531d5c9
sergeimaslov86 <sergei.maslov86@gmail.com>
2016-10-17 15:44:15 +0300

    youtube video added

---
+++ sections/extensions/en/tm_youtube_background_video.php
@@ -0,0 +1,19 @@
+<h3>TM Youtube background video module</h3>
+
+<p>TM Youtube background video is a module for adding a background youtube video to your website.</p>
+
+<h6>All the available module options are available in settings:</h6>
+
+<ol class="index-list">
+    <li>Youtube video link;</li>
+    <li>Video playback settings;</li>
+    <li>Mobile display settings;</li>
+    <li>”Play/Stop” button settings.</li>
+</ol>
+<figure class="img-polaroid">
+    <img src="img/background-video.jpg" alt="">
+</figure>
+<p class="alert alert-warning">
+In our templates TM Youtube background video content is presented by loading certain module or position, for example, "{loadmodule articles_single, About us}". In order to prevent Error 404 on your website, please make sure that the name of the loaded module coincides with the one, specified for loading. In this case, the "About us" module should be published and should not contain more than one space in the module name.
+    You can learn more about loadmodule/loadposition at the <a href="https://docs.joomla.org/How_do_you_put_a_module_inside_an_article%3F" target="_blank">official website</a>.
+</p>
