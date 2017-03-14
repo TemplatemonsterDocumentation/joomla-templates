895960e62060c0c1d8425ba119929eb34ba8cb6a
LinguistMB <MariaBerkova.MB@gmail.com>
2017-03-14 11:47:49 +0200

    Add files via upload

---
+++ sections/extensions/en/twitter.php
@@ -0,0 +1,90 @@
+<h3>TM Twitterfeed</h3>
+ 
+<p align="justify">This module will help you to show feeds from your twitter profile.</p>
+
+<figure class="img-polaroid">
+    <p><img src="img/twitterfeed-1.jpg" alt="">
+</p></figure>
+    <p>To configure the <strong>TM Twitterfeed </strong> module navigate to <strong>Extensions</strong> &gt; <strong>Modules</strong>. </p>
+<figure class="img-polaroid">
+    <p><img src="img/facebook_feed-3.jpg" alt="">
+    </p></figure>
+    <p align="justify">Press &quot;<strong>Search Tool</strong>&quot; button and select the <strong>TM Twitterfeed </strong>in the &quot;- Select Type -&quot; filter dropdown list. You will see the<strong> TM Twitterfeed </strong>Module.</p>
+    <figure class="img-polaroid">
+    <p><img src="img/twitterfeed.jpg" alt="">
+</p></figure>
+    <p>Click on module title link to be brought to the settings window of the module. </p>
+ <figure class="img-polaroid">
+    <p><img src="img/twitterfeed-5.jpg" alt=""></p></figure>
+<p>At the <strong>Module</strong> tab you may set up the following options:</p>
+    <ol class="marked-list">
+      <li><strong> Consumer key </strong> —  consumer key generated for Twitter application;</li>
+      <li><strong>Consumer secret </strong>— consumer secret key generated for Twitter application;</li>
+      <li><strong>Access token</strong> — consumer access token generated for Twitter application;</li>
+      <li><strong> Access secret</strong> — consumer access secret generated for Twitter application;</li>
+      <li><strong> Displaying tweets by a </strong>— choose if you want to display feeds by a username or hashtag;</li>
+      <li><strong> User name</strong> — set the user name the tweets will be displayed by;</li>
+      <li><strong>Hashtag</strong> — set the hashtag the tweets will be displayed by</li>
+    </ol>
+<h5>Items settings </h5>
+    <ol class="marked-list">
+      <li><strong> Number of items</strong> — the number of items to display within this module;</li>
+      <li> <strong>Columns</strong> — set the number of columns;</li>
+    </ol>
+<h5>Date format settings</h5>
+    <ol class="marked-list">
+      <li> <strong>Year format</strong> — set a year format (display full year format/display 2 last digits of the year);</li>
+      <li> <strong>Month format</strong> —  set a month format (displays full month name/displays month number);</li>
+    </ol>
+<h5> Display settings &nbsp;</h5>
+    <ol class="marked-list"><li><strong>Display avatar</strong> — whether to show the avatar;</li>
+      <li><strong> Display date </strong> — whether to show date;</li>
+      <li><strong> Display user login </strong> — whether to show the user login;</li>
+      <li> <strong>Display user name </strong> — whether to show the user name;</li>
+      <li><strong> Display tweet text</strong> — whether to show the text of the tweet;</li>
+      <li><strong> Display tweet image </strong>— whether to show the image or the tweet.</li>
+    </ol>
+<p>You have to create a twitter app for your website.</p>
+<p>Go here — <a href="https://dev.twitter.com/apps">https://dev.twitter.com/apps</a> — then click on &ldquo;Create a new application&rdquo; button on top right.</p>
+<p align="justify">A   form will open – Give a Name of your application, description and   website url you have installed our module – accept the agreement and   then click on &ldquo;Create your Twitter application&rdquo; – button at bottom.</p>
+<p align="justify">Then   you will get your Consumer Key and Consumer Secret – for access token   and access token secret click on the button &ldquo;Create my access token&rdquo; –   from bottom.</p>
+<p>You may also set up the following options for the module at the <strong>Module</strong> tab:</p>
+    <figure class="img-polaroid">
+    <p><img src="img/facebook_feed-13.jpg" alt="">
+</p></figure>
+<ol class="marked-list">
+      <li> <strong>Show Title</strong>. You can show or hide module title on display. Effect will depend on the chrome style in the template.</li>
+      <li>
+        <div align="justify"> <strong>Position</strong>. You may select a module position from the list of pre-defined positions or enter your own module position by typing the name in the field and pressing enter.</div>
+      </li>
+      <li><strong> Status</strong>. If published, this module will display on your site Frontend or Backend depending on the module.</li>
+      <li> Date for publishing [<strong>Start Publishing</strong>/<strong>Finish Publishing</strong>] An optional date to Start/Finish Publishing the module.</li>
+      <li> <strong>Access</strong>. The access level group that is allowed to view this item.</li>
+      <li> <strong>Ordering</strong>. Select the ordering.</li>
+      <li> <strong>Language</strong>. Assign a language to this module.</li>
+    </ol>
+<p align="justify">The <strong>Menu Assignment</strong> &amp; <strong>Module Permissions</strong> tabs are standard for Joomla modules. You may set up menu items for the module to appear at and define the permissions.</p>
+    <p align="justify"><strong>Menu Assignment</strong> Tab:</p>
+    <figure class="img-polaroid">
+    <p><img src="img/facebook_feed-14.jpg" alt="">
+</p></figure>
+    <p align="justify"><strong>Module Permissions</strong> Tab:</p>
+    <figure class="img-polaroid">
+    <p><img src="img/facebook_feed-15.jpg" alt="">
+</p></figure>
+<p>At the <strong>Advanced</strong> tab you will see the following options:</p>
+<figure class="img-polaroid">
+    <p><img src="img/facebook_feed-16.jpg" alt="">
+</p></figure>
+    <ol class="marked-list">
+      <li> 
+        <div align="justify"><strong>Module  Suffix</strong>. A suffix to be applied to the CSS class of the module. This allows for individual module styling.</div>
+      </li>
+      <li> <strong>Module Tag</strong>. The HTML tag for module.</li>
+      <li> <strong>Bootstrap Size</strong>. An option to specify how many columns the module will use.</li>
+      <li> <strong>Offsetting columns</strong>. Increase the left margin of a column by selected number of columns.</li>
+      <li> <strong>Header Tag</strong>. The HTML tag for module header/title.</li>
+      <li> <strong>Header Class</strong>. The CSS class for module header/title.</li>
+      <li> <strong>Module Style</strong>. Use this option to override the template style for its position.</li>
+    </ol>
+   
